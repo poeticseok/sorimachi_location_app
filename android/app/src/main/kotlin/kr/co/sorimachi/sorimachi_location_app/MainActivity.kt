b4fafedb4c342b11921ef87f769e1a4974a93ec5
김형석 <poeticseok@sorimachi.co.kr>
2019-11-18 12:26:43 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/kr/co/sorimachi/sorimachi_location_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package kr.co.sorimachi.sorimachi_location_app
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
